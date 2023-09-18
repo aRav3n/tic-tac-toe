cbff72e4353a35eb8d84a3749e238f9678ad18f2
aRav3n <24660772+aRav3n@users.noreply.github.com>
2023-09-18 12:33:21 -0600

    Write initial Player function

--- script.js
+++ script.js
@@ -21,13 +21,15 @@ const statusChange = (() => {
 const gameBoard = (() => {
     const gameboard = document.querySelector('#gameboard');
     const row1 = ['','',''];
-    const row2 = ['','X',''];
-    const row3 = ['','','O'];
+    const row2 = ['','',''];
+    const row3 = ['','',''];
     const board = [row1, row2, row3];
 
     for (let i = 0; i < 3; i++) {
         for (let j = 0; j < 3; j++) {
             const div = document.createElement('div');
+            let string = 'gameSquare-' + i + '-' + j;
+            div.setAttribute('id', string);
             div.innerHTML = board[i][j];
             gameboard.appendChild(div);
         };
@@ -37,6 +39,34 @@ const gameBoard = (() => {
         board,
     };
 })();
+
+const Player = (symbol) => {
+    const board = gameBoard.board;
+    const button = document.querySelector('#assignPlayers');
+    let name;
+    button.addEventListener('click', () => {
+        let fieldId = "#" + symbol;
+        name = document.querySelector(fieldId).value;
+    });
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            let squareId = '#gameSquare-' + i + '-' + j;
+            let square = document.querySelector(squareId);
+            square.addEventListener('click', () => {
+                if (board[i][j] != "") {
+                    board[i][j] = symbol;
+                } else {
+                    console.log('invalid play');
+                };
+            });
+        };
+    };
+
+    return {
+        name,
+        symbol,
+    }
+};
 
 statusChange.initialHide('#playing');
 statusChange.buttonHide('#newGame', '#playing', '#notPlaying');
